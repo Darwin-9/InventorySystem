99b9c02c64bc1100ab212ff043cdd0e3d65722fc
TheJhampierBoy <ortizjhampier@gmail.com>
2025-09-10 11:05:38 -0500

    fix(supplier): add JsonIgnoreProperties to prevent Hibernate serialization issues

--- InventorySystemBackend/src/main/java/com/binarycode/InventorySystemBackend/model/supplier.java
+++ InventorySystemBackend/src/main/java/com/binarycode/InventorySystemBackend/model/supplier.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "suppliers")
 @Data
 @NoArgsConstructor
